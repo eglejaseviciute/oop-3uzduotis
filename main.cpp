84b3c266335d46992e5cdacc2031835205d2f3bd
eglejaseviciute <eglejasev@gmail.com>
2024-09-25 16:42:48 +0300

    Galimybė įvesti daugiau studentų
    Programoje papildomai:
    *galima nuskaityti vartotojų įvedamus reikiamus duomenis, kai turime ne vieną, o kelis studentus;
    *galimybė neįvesti duomenų apie namų darbus ir egzaminą, jei jų nėra (išvedime bus rašoma, kad duomenų nėra)

--- main.cpp
+++ main.cpp
@@ -15,19 +15,17 @@ struct Studentas {
     string vardas;
     string pavarde;
     vector<int> namuDarbai;
-    int egzaminas;
+    int egzaminas = -1; // Pradžioje nustatome, kad egzaminas neįvestas
 };
 
-int main() {
-    Studentas studentas;
-
+void ivestiStudenta(Studentas &studentas) {
     // Ivedame studento varda ir pavarde:
     cout << "Iveskite studento varda: ";
     cin >> studentas.vardas;
     cout << "Iveskite studento pavarde: ";
     cin >> studentas.pavarde;
 
-    // Ivedame namu darbu rezultatus ir tikriname klaidas
+    // Ivedame namu darbu rezultatus ir tikriname klaidas:
     cout << "Iveskite namu darbu rezultatus (iveskite -1, kai noresite baigti):" << endl;
     int rezultatas;
     while (true) {
@@ -46,29 +44,55 @@ int main() {
         }
     }
 
-    // Ivedame egzamino rezultata
-    cout << "Iveskite egzamino rezultata: ";
+    // Ivedame egzamino rezultata arba nustatome -1, jei neįvesta
+    cout << "Iveskite egzamino rezultata (jei neturi, iveskite -1): ";
     while (!(cin >> studentas.egzaminas)) {
-        cin.clear(); 
+        cin.clear();
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
         cout << "Klaida! Prasome ivesti skaiciu: ";
     }
-
-    // Pridedame tuscia eilute po ivedimu
     cout << endl;
+}
 
-    // Spausdiname ivestus studento duomenis
+void spausdintiStudenta(const Studentas &studentas) {
+    // Spausdiname ivestus studento duomenis:
     cout << "Studentas: " << studentas.vardas << " " << studentas.pavarde << endl;
 
-    // Spausdiname namu darbu rezultatus
-    cout << "Namu darbu rezultatai: ";
-    for (int nd : studentas.namuDarbai) {
-        cout << nd << " ";
+    // Spausdiname namu darbu rezultatus arba pranesima, kad ju nera:
+    if (studentas.namuDarbai.empty()) {
+        cout << "Namu darbu rezultatai: Nera" << endl;
+    } else {
+        cout << "Namu darbu rezultatai: ";
+        for (int nd : studentas.namuDarbai) {
+            cout << nd << " ";
+        }
+        cout << endl;
     }
-    cout << endl;
 
-    // Spausdiname egzamino rezultata
-    cout << "Egzamino rezultatas: " << studentas.egzaminas << endl;
+    // Spausdiname egzamino rezultata arba pranesima, kad egzamino nera:
+    if (studentas.egzaminas == -1) {
+        cout << "Egzamino rezultatas: Nera" << endl;
+    } else {
+        cout << "Egzamino rezultatas: " << studentas.egzaminas << endl;
+    }
+}
+
+int main() {
+    int studentuSkaicius;
+    cout << "Kiek studentu norite ivesti? ";
+    cin >> studentuSkaicius;
+
+    vector<Studentas> studentai(studentuSkaicius);
+
+    for (int i = 0; i < studentuSkaicius; ++i) {
+        cout << "Iveskite " << i+1 << "-ojo studento duomenis:" << endl;
+        ivestiStudenta(studentai[i]);
+    }
+
+    for (int i = 0; i < studentuSkaicius; ++i) {
+        cout << i+1 << "-ojo studento duomenys:" << endl;
+        spausdintiStudenta(studentai[i]);
+    }
 
     return 0;
 }
