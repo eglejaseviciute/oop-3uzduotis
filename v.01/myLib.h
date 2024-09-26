87d61b7f46e46f5f4c744975abac945a62a977db
eglejaseviciute <eglejasev@gmail.com>
2024-09-26 21:13:22 +0300

    Sukurtas myLib.h failas
    Sukurtas myLib.h failas po projekto išskaidymo į skirtingus failus.

---
+++ v.01/myLib.h
@@ -0,0 +1,37 @@
+#ifndef MYLIB_H_INCLUDED
+#define MYLIB_H_INCLUDED
+
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <limits>
+#include <iomanip>
+#include <algorithm>
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+#include <sstream>
+#include <cctype>
+
+
+using std::endl;
+using std::cout;
+using std::cin;
+using std::string;
+using std::vector;
+using std::numeric_limits;
+using std::streamsize;
+using std::left;
+using std::right;
+using std::setw;
+using std::setprecision;
+using std::fixed;
+using std::getline;
+using std::ifstream;
+using std::stringstream;
+using std::stoi;
+using std::sort;
+
+
+#endif // MYLIB_H_INCLUDED
