edcdaa1b25d2e3d020412ee1c855e5758c884fc3
eglejaseviciute <eglejasev@gmail.com>
2024-09-26 21:15:13 +0300

    Sukurtas studentas.h failas
    Sukurtas studentas.h failas po projekto išskaidymo į skirtingus failus.

---
+++ v.01/studentas.h
@@ -0,0 +1,16 @@
+#ifndef STUDENTAS_H
+#define STUDENTAS_H
+
+
+#include "rezultatai.h"
+#include "myLib.h"
+
+
+struct Studentas {
+    string vardas;
+    string pavarde;
+    Rezultatai rezultatai;
+};
+
+
+#endif // STUDENTAS_H
