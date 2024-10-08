493d9b3cb4b4d0f741a1edb971f2b0c01b1ac5ed
eglejaseviciute <eglejasev@gmail.com>
2024-10-09 01:07:27 +0300

    Pridėti kreipiniai į funkcijas
    Pridėta:
    * "gautiTinkamaSymboli"
    * "rodytiGreicioAnalizesRezultatus"
    * "atliktiGreicioAnalize"

--- v0.2/funkcijos.h
+++ v0.2/funkcijos.h
@@ -6,17 +6,30 @@
 #include "studentas.h"
 
 
+// Strukturos aprasymas veikimo greicio analizes matavimams
+struct VeikimoGreicioMatavimai {
+    double kurimoLaikas = 0.0;
+    double skaitymoLaikas = 0.0;
+    double rusiavimoLaikas = 0.0;
+    double rasymoLaikasVargsiukai = 0.0;
+    double rasymoLaikasGalvociai = 0.0;
+};
+
+
 double skaiciuotiVidurki(const vector<int>& namuDarbai);
 double skaiciuotiMediana(vector<int> namuDarbai);
+char gautiTinkamaSymboli(const string &pranesimas, const string &tinkamisymboliai);
 void generuotiRezultatus(Studentas &studentas, int namuDarbaiKiekis);
-void nuskaitytiDuomenisIsFailo(string &failoPavadinimas, vector<Studentas> &studentai);
+double skaiciuotiGalutiniBala(const Studentas &studentas, bool naudotiVidurki);
+double nuskaitytiDuomenisIsFailo(const string& failoPavadinimas, vector<Studentas>& studentai);
 bool lygintiPagalPavarde(const Studentas &a, const Studentas &b);
 bool lygintiPagalVarda(const Studentas &a, const Studentas &b);
 bool lygintiPagalVardaIrPavarde(const Studentas &a, const Studentas &b);
-double skaiciuotiGalutiniBala(const Studentas &studentas, bool naudotiVidurki);
-void generuotiFailus(int studentuKiekis, int namuDarbaiKiekis, const string &filePrefix);
-void procesarArchivosBucle();
+double generuotiFailus(int studentuKiekis, int namuDarbaiKiekis, const string &filePrefix);
 void rodytiRezultatus(const vector<Studentas>& studentai, bool naudotiVidurki);
+void rodytiGreicioAnalizesRezultatus(const string& failoPavadinimas);
+void failuApdorojimoCiklas();
+void atliktiGreicioAnalize();
 
 
 #endif // FUNKCIJOS_H
