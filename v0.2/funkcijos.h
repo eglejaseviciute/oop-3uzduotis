650a92d6d3afcbf951558c76bcb69619ec3758b9
eglejaseviciute <eglejasev@gmail.com>
2024-10-09 00:56:33 +0300

    Pašalinti nereikalingi ir pridėti nauji kreipiniai į funkcijas

--- v0.2/funkcijos.h
+++ v0.2/funkcijos.h
@@ -10,13 +10,12 @@ double skaiciuotiVidurki(const vector<int>& namuDarbai);
 double skaiciuotiMediana(vector<int> namuDarbai);
 void generuotiRezultatus(Studentas &studentas, int namuDarbaiKiekis);
 void nuskaitytiDuomenisIsFailo(string &failoPavadinimas, vector<Studentas> &studentai);
-bool lygintiPagalGalutiniBalaAscending(const Studentas &a, const Studentas &b);
-bool lygintiPagalGalutiniBalaDescending(const Studentas &a, const Studentas &b);
 bool lygintiPagalPavarde(const Studentas &a, const Studentas &b);
 bool lygintiPagalVarda(const Studentas &a, const Studentas &b);
 bool lygintiPagalVardaIrPavarde(const Studentas &a, const Studentas &b);
 double skaiciuotiGalutiniBala(const Studentas &studentas, bool naudotiVidurki);
 void generuotiFailus(int studentuKiekis, int namuDarbaiKiekis, const string &filePrefix);
+void procesarArchivosBucle();
 void rodytiRezultatus(const vector<Studentas>& studentai, bool naudotiVidurki);
 
 
