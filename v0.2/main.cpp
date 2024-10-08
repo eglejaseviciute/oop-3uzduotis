6760912ede2ae66a9e74f23abd1626889cfa0f27
eglejaseviciute <eglejasev@gmail.com>
2024-10-09 00:38:12 +0300

    Pridėta galimybė rinktis
    Pridėta galimybė rinktis, ar norima matyti nuskaitytus failo duomenų rezultatus ant ekrano, ar juos išspausdinti į du atskirus failus.

--- v0.2/main.cpp
+++ v0.2/main.cpp
@@ -3,14 +3,16 @@
 #include "funkcijos.h"
 
 int main() {
-    srand(static_cast<unsigned>(time(0)));
+    srand(static_cast<unsigned>(time(0))); 
 
     vector<Studentas> studentai;
     char pasirinkimas;
 
-    // Pagrindiniu pasirinkimu logika
+    // Pagrindinis programos ciklas
     while (true) {
-        cout << "Ar norite generuoti failus (g) ar nuskaityti duomenis is failo (f) ar juos ivesti patys (i)? (iveskite g arba f, arba i): ";
+        studentai.clear(); // Isvalome vektoriu kiekvienai naujai operacijai
+
+        cout << "\nAr norite generuoti failus (g), nuskaityti duomenis is failo (f), ar juos ivesti patys (i)? (iveskite g, f, arba i): ";
         cin >> pasirinkimas;
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
@@ -20,7 +22,7 @@ int main() {
             cout << "Kiek namu darbu rezultatu norite generuoti? ";
             cin >> namuDarbaiKiekis;
 
-            cout << "Kiek studentu norite generuoti? (ivesti 1000, 10000, 100000, 1000000, 10000000, 'visi'): ";
+            cout << "Kiek studentu norite generuoti? (ivesti 1000, 10000, 100000, 1000000, 10000000 arba 'visi'): ";
             string input;
             cin >> input;
 
@@ -33,7 +35,7 @@ int main() {
                 int studentuKiekis = std::stoi(input);
                 generuotiFailus(studentuKiekis, namuDarbaiKiekis, "studentai_");
             }
-            continue;
+            continue; // Griztame i pagrindini meniu
         } else if (pasirinkimas == 'f' || pasirinkimas == 'i') {
             break;
         } else {
@@ -48,6 +50,47 @@ int main() {
             cout << "Iveskite failo pavadinima (pvz., studentai.txt): ";
             getline(cin, failoPavadinimas);
             nuskaitytiDuomenisIsFailo(failoPavadinimas, studentai);
+
+            // Skaiciavimo metodo pasirinkimas
+            char metodoPasirinkimas;
+            while (true) {
+                cout << "Ar norite skaiciuoti galutini bala pagal vidurki (v) ar mediana (m)? (iveskite v arba m): ";
+                cin >> metodoPasirinkimas;
+
+                if (metodoPasirinkimas == 'v' || metodoPasirinkimas == 'm') {
+                    break; 
+                } else {
+                    cout << "Klaida! Prasome ivesti 'v' arba 'm'!" << endl;
+                }
+            }
+
+            bool naudotiVidurki = (metodoPasirinkimas == 'v');
+
+            // Rezultatu isvedimo pasirinkimas
+            char outputPasirinkimas;
+            while (true) {
+                cout << "Ar norite matyti rezultatus ekrane (e) ar surusiuosti studentus i dvi kategorijas ir irasyti i failus (f)? (iveskite e arba f): ";
+                cin >> outputPasirinkimas;
+
+                if (outputPasirinkimas == 'e' || outputPasirinkimas == 'f') {
+                    break;
+                } else {
+                    cout << "Klaida! Prasome ivesti 'e' arba 'f'!" << endl;
+                }
+            }
+
+            if (outputPasirinkimas == 'e') {
+                rodytiRezultatus(studentai, naudotiVidurki);
+            } else {
+                vector<Studentas> vargsiukai, galvociai;
+                rusiuotiStudentus(studentai, vargsiukai, galvociai, naudotiVidurki);
+
+                rasytiStudentusIFaila(vargsiukai, "vargsiukai.txt", naudotiVidurki);
+                rasytiStudentusIFaila(galvociai, "galvociai.txt", naudotiVidurki);
+
+                cout << "Studentai buvo surusiuoti ir irasyti i failus 'vargsiukai.txt' ir 'galvociai.txt'." << endl;
+            }
+
         } else if (pasirinkimas == 'i') {
             // Rankinio ivedimo logika
             int studentuSkaicius;
@@ -68,7 +111,6 @@ int main() {
                 cout << endl;
                 cout << "Iveskite " << i + 1 << "-ojo studento duomenis:" << endl;
 
-                // Generate random results?
                 char atsitiktiniuPasirinkimas;
                 while (true) {
                     cout << "Ar norite generuoti atsitiktinius balus (t - TAIP, n - NE)? ";
@@ -104,37 +146,27 @@ int main() {
                 ivestiStudenta(studentas, atsitiktiniai, namuDarbaiKiekis);
                 studentai.push_back(studentas);
             }
+
+            char metodoPasirinkimas;
+            while (true) {
+                cout << "Ar norite skaiciuoti galutini bala pagal vidurki (v) ar mediana (m)? (iveskite v arba m): ";
+                cin >> metodoPasirinkimas;
+
+                if (metodoPasirinkimas == 'v' || metodoPasirinkimas == 'm') {
+                    break; 
+                } else {
+                    cout << "Klaida! Prasome ivesti 'v' arba 'm'!" << endl;
+                }
+            }
+
+            bool naudotiVidurki = (metodoPasirinkimas == 'v');
+
+            std::sort(studentai.begin(), studentai.end(), lygintiPagalVarda);
+
+            rodytiRezultatus(studentai, naudotiVidurki);
         }
     } catch (const std::exception &e) {
         cout << e.what() << endl;
-    }
-
-    // Galutinio balo skaiciavimo metodo pasirinkimas
-    char metodoPasirinkimas;
-    while (true) {
-        cout << "Ar norite skaiciuoti galutini bala pagal vidurki (v) ar mediana (m)? (iveskite v arba m): ";
-        cin >> metodoPasirinkimas;
-
-        if (metodoPasirinkimas == 'v' || metodoPasirinkimas == 'm') {
-            break;
-        } else {
-            cout << "Klaida! Prasome ivesti 'v' arba 'm'!" << endl;
-        }
-    }
-
-    bool naudotiVidurki = (metodoPasirinkimas == 'v');
-
-    // Studentu rusiavimas ir spausdinimas
-    std::sort(studentai.begin(), studentai.end(), lygintiPagalVarda);
-
-    cout << endl;
-    cout << left << setw(18) << "Vardas"
-         << setw(18) << "Pavarde"
-         << (naudotiVidurki ? "Galutinis (Vid.)" : "Galutinis (Med.)") << endl;
-    cout << "--------------------------------------------------------------" << endl;
-
-    for (const auto &studentas : studentai) {
-        spausdintiStudenta(studentas, naudotiVidurki);
     }
 
     return 0;
