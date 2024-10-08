d0c15acc66a9597c40b7cb1ae666114d69b2893f
eglejaseviciute <eglejasev@gmail.com>
2024-10-09 00:52:09 +0300

    Pataisytas kodas
    Pataisytas studentų duomenų rūšiavimas.

--- v0.2/main.cpp
+++ v0.2/main.cpp
@@ -3,55 +3,73 @@
 #include "funkcijos.h"
 
 int main() {
-    srand(static_cast<unsigned>(time(0))); 
+    srand(static_cast<unsigned>(time(0)));
 
     vector<Studentas> studentai;
     char pasirinkimas;
 
-    // Pagrindinis programos ciklas
     while (true) {
-        studentai.clear(); // Isvalome vektoriu kiekvienai naujai operacijai
+        studentai.clear();
 
-        cout << "\nAr norite generuoti failus (g), nuskaityti duomenis is failo (f), ar juos ivesti patys (i)? (iveskite g, f, arba i): ";
+        cout << "\nAr norite generuoti failus (g) ar nuskaityti duomenis is failo (f), ar juos ivesti patys (i)? (iveskite g arba f, arba i): ";
         cin >> pasirinkimas;
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
         if (pasirinkimas == 'g') {
-            // Failu generavimo logika
             int namuDarbaiKiekis;
-            cout << "Kiek namu darbu rezultatu norite generuoti? ";
-            cin >> namuDarbaiKiekis;
-
-            cout << "Kiek studentu norite generuoti? (ivesti 1000, 10000, 100000, 1000000, 10000000 arba 'visi'): ";
-            string input;
-            cin >> input;
-
-            if (input == "visi") {
-                int sizes[] = {1000, 10000, 100000, 1000000, 10000000};
-                for (int size : sizes) {
-                    generuotiFailus(size, namuDarbaiKiekis, "studentai_");
+            while (true) {
+                cout << "Kiek namu darbu rezultatu norite generuoti? ";
+                if (cin >> namuDarbaiKiekis && namuDarbaiKiekis > 0) {
+                    break;
+                } else {
+                    cout << "Klaida! Iveskite teigiama skaiciu." << endl;
+                    cin.clear();
+                    cin.ignore(numeric_limits<streamsize>::max(), '\n');
                 }
-            } else {
-                int studentuKiekis = std::stoi(input);
-                generuotiFailus(studentuKiekis, namuDarbaiKiekis, "studentai_");
             }
-            continue; // Griztame i pagrindini meniu
+
+            string input;
+            while (true) {
+                cout << "Kiek studentu norite generuoti? (ivesti 1000, 10000, 100000, 1000000, 10000000 arba 'visi'): ";
+                cin >> input;
+
+                if (input == "visi") {
+                    int sizes[] = {1000, 10000, 100000, 1000000, 10000000};
+                    for (int size : sizes) {
+                        generuotiFailus(size, namuDarbaiKiekis, "studentai_");
+                    }
+                    break;
+                } else {
+                    try {
+                        int studentuKiekis = std::stoi(input);
+                        if (studentuKiekis > 0) {
+                            generuotiFailus(studentuKiekis, namuDarbaiKiekis, "studentai_");
+                            break;
+                        } else {
+                            cout << "Klaida! Iveskite teigiama skaiciu." << endl;
+                        }
+                    } catch (const std::invalid_argument&) {
+                        cout << "Klaida! Iveskite tinkama skaiciu arba 'visi'." << endl;
+                    } catch (const std::out_of_range&) {
+                        cout << "Klaida! Skaicius per didelis. Bandykite dar karta." << endl;
+                    }
+                }
+            }
+            continue;
         } else if (pasirinkimas == 'f' || pasirinkimas == 'i') {
             break;
         } else {
             cout << "Klaida! Prasome ivesti 'g', 'f' arba 'i'!" << endl;
         }
     }
-
     try {
         if (pasirinkimas == 'f') {
-            // Skaitymo is failo logika
             string failoPavadinimas;
             cout << "Iveskite failo pavadinima (pvz., studentai.txt): ";
             getline(cin, failoPavadinimas);
             nuskaitytiDuomenisIsFailo(failoPavadinimas, studentai);
 
-            // Skaiciavimo metodo pasirinkimas
+            
             char metodoPasirinkimas;
             while (true) {
                 cout << "Ar norite skaiciuoti galutini bala pagal vidurki (v) ar mediana (m)? (iveskite v arba m): ";
@@ -66,7 +84,7 @@ int main() {
 
             bool naudotiVidurki = (metodoPasirinkimas == 'v');
 
-            // Rezultatu isvedimo pasirinkimas
+            
             char outputPasirinkimas;
             while (true) {
                 cout << "Ar norite matyti rezultatus ekrane (e) ar surusiuosti studentus i dvi kategorijas ir irasyti i failus (f)? (iveskite e arba f): ";
@@ -80,8 +98,27 @@ int main() {
             }
 
             if (outputPasirinkimas == 'e') {
+                
+                std::sort(studentai.begin(), studentai.end(), lygintiPagalVardaIrPavarde);
+                
                 rodytiRezultatus(studentai, naudotiVidurki);
             } else {
+                char sortingPreference;
+                cout << "Kaip norite surusiuoti studentus? (1 - Pagal galutini bala didejimo tvarka, 2 - Pagal galutini bala mazejimo tvarka, 3 - Pagal varda, 4 - Pagal pavarde, 5 - Pagal varda ir pavarde): ";
+                cin >> sortingPreference;
+
+                if (sortingPreference == '1') {
+                    std::sort(studentai.begin(), studentai.end(), lygintiPagalGalutiniBalaAscending);
+                } else if (sortingPreference == '2') {
+                    std::sort(studentai.begin(), studentai.end(), lygintiPagalGalutiniBalaDescending);
+                } else if (sortingPreference == '3') {
+                    std::sort(studentai.begin(), studentai.end(), lygintiPagalVarda);
+                } else if (sortingPreference == '4') {
+                    std::sort(studentai.begin(), studentai.end(), lygintiPagalPavarde);
+                } else if (sortingPreference == '5') {
+                    std::sort(studentai.begin(), studentai.end(), lygintiPagalVardaIrPavarde);
+                }
+
                 vector<Studentas> vargsiukai, galvociai;
                 rusiuotiStudentus(studentai, vargsiukai, galvociai, naudotiVidurki);
 
@@ -92,7 +129,6 @@ int main() {
             }
 
         } else if (pasirinkimas == 'i') {
-            // Rankinio ivedimo logika
             int studentuSkaicius;
             while (true) {
                 cout << "Kiek studentu norite ivesti? ";
@@ -147,6 +183,7 @@ int main() {
                 studentai.push_back(studentas);
             }
 
+            
             char metodoPasirinkimas;
             while (true) {
                 cout << "Ar norite skaiciuoti galutini bala pagal vidurki (v) ar mediana (m)? (iveskite v arba m): ";
@@ -161,8 +198,10 @@ int main() {
 
             bool naudotiVidurki = (metodoPasirinkimas == 'v');
 
-            std::sort(studentai.begin(), studentai.end(), lygintiPagalVarda);
+            
+            std::sort(studentai.begin(), studentai.end(), lygintiPagalVardaIrPavarde);
 
+            
             rodytiRezultatus(studentai, naudotiVidurki);
         }
     } catch (const std::exception &e) {
