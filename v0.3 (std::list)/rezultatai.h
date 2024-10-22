a8006d05af40c78eaa632f67ea589425752b8747
eglejaseviciute <eglejasev@gmail.com>
2024-10-22 17:44:06 +0300

    rezultatai.h iš std::vector pakeistas į std::list

---
+++ v0.3 (std::list)/rezultatai.h
@@ -0,0 +1,15 @@
+#ifndef REZULTATAI_H
+#define REZULTATAI_H
+
+
+#include "myLib.h"
+
+
+// Strukturos aprasymas rezultatams
+struct Rezultatai {
+    list<int> namuDarbai;
+    int egzaminas;
+};
+
+
+#endif // REZULTATAI_H
