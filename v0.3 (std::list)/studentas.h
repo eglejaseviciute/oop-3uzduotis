7cd9e91041ecc72a00a3ebcea4801785ddfa13b9
eglejaseviciute <eglejasev@gmail.com>
2024-10-22 17:44:53 +0300

    studentas.h iš std::vector pakeistas į std::list

---
+++ v0.3 (std::list)/studentas.h
@@ -0,0 +1,23 @@
+#ifndef STUDENTAS_H
+#define STUDENTAS_H
+
+
+#include "rezultatai.h"
+#include "myLib.h"
+
+
+// Strukturos aprasymas studentui
+struct Studentas {
+    string vardas;
+    string pavarde;
+    Rezultatai rezultatai;
+};
+
+
+void ivestiStudenta(Studentas &studentas, bool atsitiktiniai, int namuDarbaiKiekis);
+void spausdintiStudenta(const Studentas &studentas, bool naudotiVidurki);
+double rusiuotiStudentus(const list<Studentas> &studentai, list<Studentas> &vargsiukai, list<Studentas> &galvociai, bool naudotiVidurki);
+double rasytiStudentusIFaila(const list<Studentas> &studentai, const string &failoPavadinimas, bool naudotiVidurki);
+
+
+#endif // STUDENTAS_H
