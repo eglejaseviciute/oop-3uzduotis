6958b7c42483a07bac63cbe8acdc5601b6f8ceb5
eglejaseviciute <eglejasev@gmail.com>
2024-10-22 17:43:41 +0300

    myLib.h iš std::vector pakeistas į std::list

---
+++ v0.3 (std::list)/myLib.h
@@ -0,0 +1,49 @@
+#ifndef MYLIB_H
+#define MYLIB_H
+
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <limits>
+#include <iomanip>
+#include <algorithm>
+#include <cstdlib>
+#include <fstream>
+#include <sstream>
+#include <cctype>
+#include <chrono>
+#include <functional>
+#include <map>
+#include <stdexcept>
+#include <list>
+
+
+using std::endl;
+using std::cout;
+using std::cin;
+using std::string;
+using std::vector;
+using std::numeric_limits;
+using std::streamsize;
+using std::left;
+using std::right;
+using std::setw;
+using std::setprecision;
+using std::fixed;
+using std::getline;
+using std::map;
+using std::stoi;
+using std::invalid_argument;
+using std::out_of_range;
+using std::ifstream;
+using std::runtime_error;
+using std::stringstream;
+using std::to_string;
+using std::ofstream;
+using std::exception;
+using std::advance;
+using std::list;
+
+
+#endif // MYLIB_H
