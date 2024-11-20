f39d5bf725d5e0ce8aec6a168941970270c2e4b8
eglejaseviciute <eglejasev@gmail.com>
2024-11-20 22:01:04 +0200

    Sukurti nauji failai

---
+++ Class versija/List/Include/strategijos.h
@@ -0,0 +1,29 @@
+#ifndef STRATEGIJOS_H
+#define STRATEGIJOS_H
+
+#include "myLib.h"
+#include "studentas.h"
+
+
+// Strategiju funkciju deklaracijos
+double rusiuotiStudentusStrategija1(const list<Studentas>& studentai, 
+                                   list<Studentas>& vargsiukai, 
+                                   list<Studentas>& galvociai, 
+                                   bool naudotiVidurki);
+
+double rusiuotiStudentusStrategija2(list<Studentas>& studentai, 
+                                   list<Studentas>& vargsiukai, 
+                                   bool naudotiVidurki);
+
+double rusiuotiStudentusStrategija3(list<Studentas>& studentai, 
+                                   list<Studentas>& vargsiukai, 
+                                   bool naudotiVidurki);
+
+void testuotiStrategija(const string& strategijaPavadinimas,
+                       list<Studentas>& studentai,
+                       bool naudotiVidurki);
+
+void palygintiStrategijas(list<Studentas>& studentai, bool naudotiVidurki);
+
+
+#endif // STRATEGIJOS_H
---
+++ Class versija/List/Include/studentas.h
@@ -0,0 +1,81 @@
+#ifndef STUDENTAS_H
+#define STUDENTAS_H
+
+#include <string>
+#include <list>
+#include <iostream>
+#include <iomanip>
+#include "rezultatai.h"
+
+
+// Studentas klase: reprezentuoja individualu studenta
+class Studentas {
+private:
+    string vardas_;
+    string pavarde_;
+    Rezultatai rezultatai_;
+
+public:
+    // Konstruktoriai
+    // Numatytasis konstruktorius
+    Studentas() = default;
+    // Konstruktorius su vardu ir pavarde
+    Studentas(const string& vardas, const string& pavarde);
+
+
+    // Destruktorius, automatiškai atlaisvina klasės išteklius
+    ~Studentas() = default;
+
+
+    // Getteriai, leidzia saugiai pasiekti studento duomenis
+    inline string vardas() const { return vardas_; }
+    inline string pavarde() const { return pavarde_; }
+    inline const Rezultatai& rezultatai() const { return rezultatai_; }
+    
+    // Setteriai, leidzia keisti studento duomenis
+    void setVardas(const string& vardas) { vardas_ = vardas; }
+    void setPavarde(const string& pavarde) { pavarde_ = pavarde; }
+    void setRezultatai(const Rezultatai& rezultatai) { rezultatai_ = rezultatai; }
+    
+    // Metodai
+    void ivestiStudenta(bool atsitiktiniai, int namuDarbaiKiekis);
+    void spausdintiStudenta(bool naudotiVidurki) const;
+    double skaiciuotiGalutiniBala(bool naudotiVidurki) const;
+};
+
+
+// Rusiavimo funkcijos
+bool lygintiPagalPavarde(const Studentas& a, const Studentas& b);
+bool lygintiPagalVarda(const Studentas& a, const Studentas& b);
+bool lygintiPagalVardaIrPavarde(const Studentas& a, const Studentas& b);
+
+
+// Failo rasymo funkcija
+double rasytiStudentusIFaila(const list<Studentas>& studentai, 
+                            const string& failoPavadinimas, 
+                            bool naudotiVidurki);
+
+
+// Rusiavimo klases
+class RusiuotiPagalGalutiniBalaDidejanciai {
+private:
+    bool naudotiVidurki;
+public:
+    RusiuotiPagalGalutiniBalaDidejanciai(bool naudotiVid) : naudotiVidurki(naudotiVid) {}
+    bool operator()(const Studentas& a, const Studentas& b) const {
+        return a.skaiciuotiGalutiniBala(naudotiVidurki) < b.skaiciuotiGalutiniBala(naudotiVidurki);
+    }
+};
+
+class RusiuotiPagalGalutiniBalaMazejanciai {
+private:
+    bool naudotiVidurki;
+public:
+    RusiuotiPagalGalutiniBalaMazejanciai(bool naudotiVid) : naudotiVidurki(naudotiVid) {}
+    bool operator()(const Studentas& a, const Studentas& b) const {
+        return a.skaiciuotiGalutiniBala(naudotiVidurki) > b.skaiciuotiGalutiniBala(naudotiVidurki);
+    }
+};
+
+
+#endif // STUDENTAS_H
