4aaf745b240e2960ece6aefc3bb929546e7e6d68
eglejaseviciute <eglejasev@gmail.com>
2024-11-20 21:01:08 +0200

    Pakeistas failas

--- Class versija/Vector/src/studentas.cpp
+++ Class versija/Vector/src/studentas.cpp
@@ -3,75 +3,75 @@
 #include "funkcijos.h"
 
 
-Studentas::Studentas(const std::string& vardas, const std::string& pavarde)
+Studentas::Studentas(const string& vardas, const string& pavarde)
     : vardas_(vardas), pavarde_(pavarde) {}
 
 
 
 // Funkcija ivedanti studento duomenis
 void Studentas::ivestiStudenta(bool atsitiktiniai, int namuDarbaiKiekis) {
-    std::cout << "Iveskite studento varda: ";
-    std::cin >> vardas_;
-    std::cout << "Iveskite studento pavarde: ";
-    std::cin >> pavarde_;
-    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    cout << "Iveskite studento varda: ";
+    cin >> vardas_;
+    cout << "Iveskite studento pavarde: ";
+    cin >> pavarde_;
+    cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
     if (atsitiktiniai) {
         // Assuming generuotiRezultatus is modified to work with the new class
         generuotiRezultatus(*this, namuDarbaiKiekis);
     } else {
-        std::string input;
+        string input;
         int rezultatas;
         bool bentVienasRezultatas = false;
 
-        std::cout << "Iveskite namu darbu rezultatus (du kartus paspauskite ENTER, kai noresite baigti):" << std::endl;
+        cout << "Iveskite namu darbu rezultatus (du kartus paspauskite ENTER, kai noresite baigti):" << endl;
 
         while (true) {
-            std::getline(std::cin, input);
+            getline(cin, input);
 
             if (input.empty()) {
                 if (bentVienasRezultatas) break;
-                std::cout << "Turite ivesti bent viena namu darbu rezultata!" << std::endl;
+                cout << "Turite ivesti bent viena namu darbu rezultata!" << endl;
                 continue;
             }
 
             try {
-                rezultatas = std::stoi(input);
+                rezultatas = stoi(input);
                 if (rezultatas < 0 || rezultatas > 10) {
-                    std::cout << "Klaida! Namu darbu rezultatas turi buti tarp 0 ir 10. Iveskite teigiama skaiciu: ";
+                    cout << "Klaida! Namu darbu rezultatas turi buti tarp 0 ir 10. Iveskite teigiama skaiciu: ";
                 } else {
                     rezultatai_.addNamuDarbas(rezultatas);
                     bentVienasRezultatas = true;
                 }
             } catch (const std::invalid_argument&) {
-                std::cout << "Klaida! Prasome ivesti tinkama skaiciu: ";
+                cout << "Klaida! Prasome ivesti tinkama skaiciu: ";
             } catch (const std::out_of_range&) {
-                std::cout << "Klaida! Skaicius per didelis. Bandykite dar karta: ";
+                cout << "Klaida! Skaicius per didelis. Bandykite dar karta: ";
             }
         }
 
-        std::cout << "Iveskite egzamino rezultata: ";
+        cout << "Iveskite egzamino rezultata: ";
         while (true) {
-            std::getline(std::cin, input);
+            getline(std::cin, input);
             if (input.empty()) {
-                std::cout << "Klaida! Prasome ivesti egzamino rezultata: ";
+                cout << "Klaida! Prasome ivesti egzamino rezultata: ";
             } else {
                 try {
-                    int egz = std::stoi(input);
+                    int egz = stoi(input);
                     if (egz < 0 || egz > 10) {
-                        std::cout << "Klaida! Egzamino rezultatas turi buti tarp 0 ir 10. Iveskite tinkama skaiciu: ";
+                        cout << "Klaida! Egzamino rezultatas turi buti tarp 0 ir 10. Iveskite tinkama skaiciu: ";
                     } else {
                         rezultatai_.setEgzaminas(egz);
                         break;
                     }
-                } catch (const std::invalid_argument&) {
-                    std::cout << "Klaida! Prasome ivesti tinkama skaiciu: ";
-                } catch (const std::out_of_range&) {
-                    std::cout << "Klaida! Skaicius per didelis. Bandykite dar karta: ";
+                } catch (const invalid_argument&) {
+                    cout << "Klaida! Prasome ivesti tinkama skaiciu: ";
+                } catch (const out_of_range&) {
+                    cout << "Klaida! Skaicius per didelis. Bandykite dar karta: ";
                 }
             }
         }
-        std::cout << std::endl;
+        cout << endl;
     }
 }
 
@@ -79,10 +79,10 @@ void Studentas::ivestiStudenta(bool atsitiktiniai, int namuDarbaiKiekis) {
 // Funkcija spausdinanti studento duomenis
 void Studentas::spausdintiStudenta(bool naudotiVidurki) const {
     double galutinis = skaiciuotiGalutiniBala(naudotiVidurki);
-    std::cout << std::left << std::setw(18) << vardas_
-              << std::setw(18) << pavarde_
-              << std::left << std::setw(15) << std::fixed 
-              << std::setprecision(2) << galutinis << std::endl;
+    cout << sleft << setw(18) << vardas_
+              << setw(18) << pavarde_
+              << left << setw(15) << fixed
+              << setprecision(2) << galutinis << endl;
 }
 
 
@@ -97,24 +97,24 @@ double Studentas::skaiciuotiGalutiniBala(bool naudotiVidurki) const {
 
 
 // Funkcija rasanti studentus i faila
-double rasytiStudentusIFaila(const std::vector<Studentas>& studentai,
-                            const std::string& failoPavadinimas, 
+double rasytiStudentusIFaila(const vector<Studentas>& studentai,
+                            const string& failoPavadinimas,
                             bool naudotiVidurki) {
     auto start = std::chrono::high_resolution_clock::now();
-    std::ofstream failas(failoPavadinimas);
+    ofstream failas(failoPavadinimas);
     if (!failas) {
-        throw std::runtime_error("Nepavyko atidaryti failo: " + failoPavadinimas);
+        throw runtime_error("Nepavyko atidaryti failo: " + failoPavadinimas);
     }
 
-    failas << std::left << std::setw(18) << "Vardas" << std::setw(18) << "Pavarde" 
-           << (naudotiVidurki ? "Galutinis (Vid.)" : "Galutinis (Med.)") << std::endl;
-    failas << std::string(60, '-') << std::endl;
+    failas << left << setw(18) << "Vardas" << setw(18) << "Pavarde"
+           << (naudotiVidurki ? "Galutinis (Vid.)" : "Galutinis (Med.)") << endl;
+    failas << string(60, '-') << endl;
 
     for (const auto& studentas : studentai) {
         double galutinis = studentas.skaiciuotiGalutiniBala(naudotiVidurki);
-        failas << std::left << std::setw(18) << studentas.vardas()
-               << std::setw(18) << studentas.pavarde()
-               << std::fixed << std::setprecision(2) << galutinis << std::endl;
+        failas << left << setw(18) << studentas.vardas()
+               << setw(18) << studentas.pavarde()
+               << fixed << setprecision(2) << galutinis << endl;
     }
 
     failas.close();
