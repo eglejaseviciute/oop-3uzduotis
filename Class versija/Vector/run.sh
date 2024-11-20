25ab854261a9f53609be0447e77828689c7f4d37
eglejaseviciute <eglejasev@gmail.com>
2024-11-20 20:00:25 +0200

    Pataisytas kodas

--- Class versija/Vector/run.sh
+++ Class versija/Vector/run.sh
@@ -3,12 +3,12 @@ cmake --build . --config Release
 
 
 echo "Executable Sizes:"
-ls -lh v1.0_vector_O1 v1.0_vector_O2 v1.0_vector_O3
+ls -lh Vector_O1 Vector_O2 Vector_O3
 
 echo "Performance Measurements:"
 for opt in O1 O2 O3; do
     echo "Optimization Level: $opt"
-    time ./v1.0_vector_${opt}
+    time ./Vector_${opt}
 done
 
 echo "Experiment completed. Press enter to close."
