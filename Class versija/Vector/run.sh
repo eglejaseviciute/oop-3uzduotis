8b0f5c38b51cfedb5b0dd29743e000389874f541
eglejaseviciute <eglejasev@gmail.com>
2024-11-21 15:52:35 +0200

    Patvarkytas run.sh failas

--- Class versija/Vector/run.sh
+++ Class versija/Vector/run.sh
@@ -1,10 +1,6 @@
 cmake -DCMAKE_BUILD_TYPE=Release .
 cmake --build . --config Release
 
-
-echo "Executable Sizes:"
-ls -lh Vector_O1 Vector_O2 Vector_O3
-
 echo "Performance Measurements:"
 for opt in O1 O2 O3; do
     echo "Optimization Level: $opt"
