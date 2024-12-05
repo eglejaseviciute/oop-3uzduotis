1b2437c800f457428b9d6728e335361e893dc323
eglejaseviciute <eglejasev@gmail.com>
2024-12-05 15:52:32 +0200

    Pakeistas failas

--- Class versija/Vector/src/studentas.cpp
+++ Class versija/Vector/src/studentas.cpp
@@ -3,21 +3,23 @@
 #include "funkcijos.h"
 
 
-Studentas::Studentas(const string& vardas, const string& pavarde)
-    : vardas_(vardas), pavarde_(pavarde) {}
+istream& operator>>(istream& is, Studentas& studentas) {
+    studentas.clearNamuDarbai();
 
+    // Aptinkama, ar ivestis vyksta per konsole, ar is failo
+    bool isConsoleInput = (&is == &cin);
 
-// Funkcija ivedanti studento duomenis
-void Studentas::ivestiStudenta(bool atsitiktiniai, int namuDarbaiKiekis) {
-    cout << "Iveskite studento varda: ";
-    cin >> vardas_;
-    cout << "Iveskite studento pavarde: ";
-    cin >> pavarde_;
-    cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    if (isConsoleInput) {
+        // Konsoles ivedimo logika
+        cout << "\n";
+        cout << "Iveskite studento varda: ";
+        cin >> studentas.vardas_;
+    
+        cout << "Iveskite studento pavarde: ";
+        cin >> studentas.pavarde_;
 
-    if (atsitiktiniai) {
-        generuotiRezultatus(*this, namuDarbaiKiekis);
-    } else {
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
         string input;
         int rezultatas;
         bool bentVienasRezultatas = false;
@@ -25,7 +27,7 @@ void Studentas::ivestiStudenta(bool atsitiktiniai, int namuDarbaiKiekis) {
         cout << "Iveskite namu darbu rezultatus (du kartus paspauskite ENTER, kai noresite baigti):" << endl;
 
         while (true) {
-            getline(cin, input);
+            getline(is, input);
 
             if (input.empty()) {
                 if (bentVienasRezultatas) break;
@@ -34,103 +36,146 @@ void Studentas::ivestiStudenta(bool atsitiktiniai, int namuDarbaiKiekis) {
             }
 
             try {
-                rezultatas = stoi(input);
+                rezultatas = stoi(input); // Konvertuojama i sveikaji skaiciu
                 if (rezultatas < 0 || rezultatas > 10) {
                     cout << "Klaida! Namu darbu rezultatas turi buti tarp 0 ir 10. Iveskite teigiama skaiciu: ";
-                } else {
-                    rezultatai_.addNamuDarbas(rezultatas);
-                    bentVienasRezultatas = true;
-                }
-            } catch (const std::invalid_argument&) {
+                    continue;
+                } 
+                studentas.addNamuDarbas(rezultatas);
+                bentVienasRezultatas = true;
+            } catch (const invalid_argument&) {
                 cout << "Klaida! Prasome ivesti tinkama skaiciu: ";
-            } catch (const std::out_of_range&) {
+                continue;
+            } catch (const out_of_range&) {
                 cout << "Klaida! Skaicius per didelis. Bandykite dar karta: ";
+                continue;
             }
         }
 
         cout << "Iveskite egzamino rezultata: ";
         while (true) {
-            getline(std::cin, input);
+            getline(is, input);
             if (input.empty()) {
                 cout << "Klaida! Prasome ivesti egzamino rezultata: ";
-            } else {
-                try {
-                    int egz = stoi(input);
-                    if (egz < 0 || egz > 10) {
-                        cout << "Klaida! Egzamino rezultatas turi buti tarp 0 ir 10. Iveskite tinkama skaiciu: ";
-                    } else {
-                        rezultatai_.setEgzaminas(egz);
-                        break;
-                    }
-                } catch (const invalid_argument&) {
-                    cout << "Klaida! Prasome ivesti tinkama skaiciu: ";
-                } catch (const out_of_range&) {
-                    cout << "Klaida! Skaicius per didelis. Bandykite dar karta: ";
+                continue;
+            }
+
+            try {
+                int egz = stoi(input); // Konvertuojamas egzamino rezultatas
+                if (egz < 0 || egz > 10) {
+                    cout << "Klaida! Egzamino rezultatas turi buti tarp 0 ir 10. Iveskite tinkama skaiciu: ";
+                    continue;
                 }
+                studentas.setEgzaminas(egz);
+                break;
+            } catch (const invalid_argument&) {
+                cout << "Klaida! Prasome ivesti tinkama skaiciu: ";
+            } catch (const out_of_range&) {
+                cout << "Klaida! Skaicius per didelis. Bandykite dar karta: ";
             }
         }
-        cout << endl;
+    } else {
+        // Failo ivesties logika
+        string vardas, pavarde;
+        vector<int> namuDarbai;
+        string input;
+        int nd;
+        bool egzaminasNuskaitytas = false;
+
+        if (!(is >> vardas >> pavarde)) {
+            throw runtime_error("Nepavyko nuskaityti vardo ir pavardes.");
+        }
+
+        studentas.setVardas(vardas);
+        studentas.setPavarde(pavarde);
+
+        while (is >> input) {
+            try {
+                nd = stoi(input); // Bandome konvertuoti i skaiciu
+                if (nd < 0 || nd > 10) {
+                    throw out_of_range("Rezultatas ne intervale [0, 10]");
+                }
+                if (!egzaminasNuskaitytas) {
+                    namuDarbai.push_back(nd);
+                } else {
+                    throw runtime_error("Po egzamino rezultato aptikti papildomi duomenys.");
+                }
+            } catch (const invalid_argument&) {
+                // Klaida, jei ivestas netinkamas simbolis
+                cout << "Netinkamas simbolis '" << input << "' studentui(-ei): "
+                     << studentas.vardas() << " " << studentas.pavarde() << "." << endl;
+                cout << "Iveskite tinkama rezultata (skaicius nuo 0 iki 10): ";
+
+                while (!(cin >> nd) || nd < 0 || nd > 10) {
+                    cout << "Klaida! Iveskite skaiciu tarp 0 ir 10: ";
+                    cin.clear();
+                    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                }
+                namuDarbai.push_back(nd); // Irasome pataisyta rezultata
+            } catch (const out_of_range&) {
+                // Klaida, jei rezultatas ne leistiname intervale
+                cout << "Rezultatas '" << input << "' ne intervale [0, 10] studentui(-ei): "
+                    << studentas.vardas() << " " << studentas.pavarde() << "." << endl;
+                cout << "Iveskite tinkama rezultata (skaicius nuo 0 iki 10): ";
+                while (!(cin >> nd) || nd < 0 || nd > 10) {
+                    cout << "Klaida! Iveskite skaiciu tarp 0 iki 10: ";
+                    cin.clear();
+                    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                }
+                namuDarbai.push_back(nd); // Irasome pataisyta rezultata
+            } catch (const runtime_error& e) {
+                cout << "Klaida: " << e.what() << " (" << studentas.vardas() << " " << studentas.pavarde() << ")." << endl;
+                break;
+            }
+        }
+
+        if (namuDarbai.empty()) {
+            throw runtime_error("Nerasta namu darbu rezultatu.");
+        }
+
+        // Egzamino rezultatas - paskutinis elementas namu darbu sarase
+        studentas.setEgzaminas(namuDarbai.back());
+        namuDarbai.pop_back();// Pasaliname egzamino rezultata is namu darbu saraso
+        studentas.setNamuDarbai(namuDarbai);
+
+        if (namuDarbai.empty()) {
+            throw runtime_error("Namu darbu rezultatu nera, rastas tik egzamino rezultatas.");
+        }
     }
+    return is;
 }
 
 
-// Funkcija spausdinanti studento duomenis
-void Studentas::spausdintiStudenta(bool naudotiVidurki) const {
-    double galutinis = skaiciuotiGalutiniBala(naudotiVidurki);
-    cout << sleft << setw(18) << vardas_
-              << setw(18) << pavarde_
-              << left << setw(15) << fixed
-              << setprecision(2) << galutinis << endl;
+ostream& operator<<(ostream& os, const Studentas& studentas) {
+    os << left << setw(18) << studentas.vardas()
+       << setw(22) << studentas.pavarde();
+
+    return os;
+}
+
+
+
+// Funkcija ivedanti studento duomenis
+void Studentas::ivestiStudenta(bool atsitiktiniai, int namuDarbaiKiekis) {
+    if (atsitiktiniai) {
+        // Naudojama funkcija generuoti rezultatus
+        cout << "Iveskite studento varda: ";
+        cin >> vardas_;
+    
+        cout << "Iveskite studento pavarde: ";
+        cin >> pavarde_;
+        generuotiRezultatus(*this, namuDarbaiKiekis);
+    } else {
+        // Naudojamas operator>> rankiniams duomenims
+        cin >> *this;
+    }
 }
 
 
 // Funkcija skaiciuojanti galutini bala
 double Studentas::skaiciuotiGalutiniBala(bool naudotiVidurki) const {
-    double namuDarbuRezultatas = naudotiVidurki ? 
-        skaiciuotiVidurki(rezultatai_.namuDarbai()) : 
-        skaiciuotiMediana(rezultatai_.namuDarbai());
-    return 0.4 * namuDarbuRezultatas + 0.6 * rezultatai_.egzaminas();
-}
-
-
-// Funkcija rasanti studentus i faila
-double rasytiStudentusIFaila(const vector<Studentas>& studentai,
-                            const string& failoPavadinimas,
-                            bool naudotiVidurki) {
-    auto start = std::chrono::high_resolution_clock::now();
-    ofstream failas(failoPavadinimas);
-    if (!failas) {
-        throw runtime_error("Nepavyko atidaryti failo: " + failoPavadinimas);
-    }
-
-    failas << left << setw(18) << "Vardas" << setw(18) << "Pavarde"
-           << (naudotiVidurki ? "Galutinis (Vid.)" : "Galutinis (Med.)") << endl;
-    failas << string(60, '-') << endl;
-
-    for (const auto& studentas : studentai) {
-        double galutinis = studentas.skaiciuotiGalutiniBala(naudotiVidurki);
-        failas << left << setw(18) << studentas.vardas()
-               << setw(18) << studentas.pavarde()
-               << fixed << setprecision(2) << galutinis << endl;
-    }
-
-    failas.close();
-    auto end = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> diff = end - start;
-    return diff.count();
-}
-
-
-bool lygintiPagalPavarde(const Studentas& a, const Studentas& b) {
-    return a.pavarde() < b.pavarde();
-}
-
-
-bool lygintiPagalVarda(const Studentas& a, const Studentas& b) {
-    return a.vardas() < b.vardas();
-}
-
-
-bool lygintiPagalVardaIrPavarde(const Studentas& a, const Studentas& b) {
-    return (a.vardas() + a.pavarde()) < (b.vardas() + b.pavarde());
+    double namuDarbuRezultatas = naudotiVidurki ?
+        skaiciuotiVidurki(namuDarbai_) :
+        skaiciuotiMediana(namuDarbai_);
+    return 0.4 * namuDarbuRezultatas + 0.6 * egzaminas_;
 }
