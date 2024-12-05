07b3bed4d40d7d15c553a6c41ca2602e7a73d82b
eglejaseviciute <eglejasev@gmail.com>
2024-12-05 23:31:43 +0200

    Pakeistas failas

--- Class versija/Vector/Include/studentas.h
+++ Class versija/Vector/Include/studentas.h
@@ -2,34 +2,31 @@
 #define STUDENTAS_H
 
 
+#include "zmogus.h"
 #include "myLib.h"
 
 
-// Studentas klase: reprezentuoja individualu studenta
-class Studentas {
+// Studentas klase, kuri paveldi is Zmogus klases
+class Studentas : public Zmogus {
 private:
-    string vardas_;
-    string pavarde_;
     vector<int> namuDarbai_;
     int egzaminas_;
 
 public:
     // Konstruktorius
     Studentas(const string& vardas = "", const string& pavarde = "") 
-        : vardas_(vardas), pavarde_(pavarde), egzaminas_(0) {}
+        : Zmogus(vardas, pavarde), egzaminas_(0) {}
 
     // Copy konstruktorius (Rule of Three - 1)
     Studentas(const Studentas& other) 
-        : vardas_(other.vardas_), 
-          pavarde_(other.pavarde_), 
+        : Zmogus(other.vardas_, other.pavarde_), 
           namuDarbai_(other.namuDarbai_), 
           egzaminas_(other.egzaminas_) {}
 
     // Copy assignment operatorius (Rule of Three - 2)
     Studentas& operator=(const Studentas& other) {
         if (this != &other) {
-            vardas_ = other.vardas_;
-            pavarde_ = other.pavarde_;
+            Zmogus::operator=(other);
             namuDarbai_ = other.namuDarbai_;
             egzaminas_ = other.egzaminas_;
         }
@@ -38,20 +35,14 @@ public:
 
     // Destruktorius (Rule of Three - 3)
     ~Studentas() {
-        vardas_.clear();
-        pavarde_.clear();
         namuDarbai_.clear();
     }
 
     // Getteriai
-    inline string vardas() const { return vardas_; }
-    inline string pavarde() const { return pavarde_; }
     inline const vector<int>& namuDarbai() const { return namuDarbai_; }
     inline int egzaminas() const { return egzaminas_; }
-    
+
     // Setteriai
-    void setVardas(const string& vardas) { vardas_ = vardas; }
-    void setPavarde(const string& pavarde) { pavarde_ = pavarde; }
     void setEgzaminas(int egz) { egzaminas_ = egz; }
     void addNamuDarbas(int pazymys) { namuDarbai_.push_back(pazymys); }
     void clearNamuDarbai() { namuDarbai_.clear(); }
@@ -61,10 +52,13 @@ public:
     void ivestiStudenta(bool atsitiktiniai, int namuDarbaiKiekis);
     double skaiciuotiGalutiniBala(bool naudotiVidurki) const;
 
-    // Ivesties operatorius
-    friend istream& operator>>(istream& is, Studentas& studentas);
+    // Virtuali funkcija is Zmogus klases
+    void spausdintiInformacija() const override {
+        cout << "Studentas: " << vardas_ << " " << pavarde_ << "\n";
+    }
 
-    // Isvesties operatorius 
+    // Ivesties ir isvesties operatoriai
+    friend istream& operator>>(istream& is, Studentas& studentas);
     friend ostream& operator<<(ostream& os, const Studentas& studentas);
 };
 
