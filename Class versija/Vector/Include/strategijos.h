03f8b23b5dc3740f880f6e84f45e2244a1a30649
eglejaseviciute <eglejasev@gmail.com>
2024-12-11 20:55:23 +0200

    Patvarkytas kodas

--- Class versija/Vector/Include/strategijos.h
+++ Class versija/Vector/Include/strategijos.h
@@ -6,25 +6,86 @@
 #include "studentas.h"
 
 
-// Strategiju funkciju deklaracijos
+/**
+ * @brief Studentų rūšiavimo strategijų modulis
+ * 
+ * Šis modulis pateikia skirtingas strategijas studentų paskirstymui į 
+ * "galvočius" (turinčius įvertinimą >= 5.0) ir "vargšiukus" (turinčius įvertinimą < 5.0).
+ */
+
+/**
+ * @brief Pirma strategija: Studentų rūšiavimas naudojant du naujus konteinerius
+ * 
+ * Ši funkcija skirsto studentus į du atskirus konteinerius: 
+ * "vargšiukus" ir "galvočius", priklausomai nuo jų galutinio balo.
+ * 
+ * @param studentai Pradinis studentų vektorius
+ * @param vargsiukai Išvestinis vektorius žemesnių pažymių studentams
+ * @param galvociai Išvestinis vektorius aukštesnių pažymių studentams
+ * @param naudotiVidurki Parametras, nurodantis skaičiuoti galutinį balą su vidurkiu arba medianu
+ * @return double Funkcijos vykdymo laikas sekundėmis
+ * 
+ */
 double rusiuotiStudentusStrategija1(const vector<Studentas>& studentai, 
                                    vector<Studentas>& vargsiukai, 
                                    vector<Studentas>& galvociai, 
                                    bool naudotiVidurki);
 
+/**
+ * @brief Antra strategija: Studentų rūšiavimas naudojant vieną naują konteinerį
+ * 
+ * Ši funkcija skirsto studentus į "vargšiukus" ir pašalina "galvočius" 
+ * iš pradinio konteinerio.
+ * 
+ * @param studentai Pradinis/galutinis studentų vektorius (bus modifikuotas)
+ * @param vargsiukai Išvestinis vektorius žemesnių pažymių studentams
+ * @param naudotiVidurki Parametras, nurodantis skaičiuoti galutinį balą su vidurkiu arba medianu
+ * @return double Funkcijos vykdymo laikas sekundėmis
+ * 
+ */
 double rusiuotiStudentusStrategija2(vector<Studentas>& studentai, 
                                    vector<Studentas>& vargsiukai, 
                                    bool naudotiVidurki);
 
+/**
+ * @brief Trečia strategija: Optimizuotas studentų rūšiavimas
+ * 
+ * Ši funkcija naudoja indeksų vektorių ir stabilų rūšiavimą 
+ * efektyvesniam studentų paskirstymui.
+ * 
+ * @param studentai Pradinis/galutinis studentų vektorius (bus modifikuotas)
+ * @param vargsiukai Išvestinis vektorius žemesnių pažymių studentams
+ * @param naudotiVidurki Parametras, nurodantis skaičiuoti galutinį balą su vidurkiu arba medianu
+ * @return double Funkcijos vykdymo laikas sekundėmis
+ * 
+ */
 double rusiuotiStudentusStrategija3(vector<Studentas>& studentai, 
                                    vector<Studentas>& vargsiukai, 
                                    bool naudotiVidurki);
 
-// Testavimo ir palyginimo funkciju deklaracijos
+/**
+ * @brief Strategijos testavimo funkcija
+ * 
+ * Ši funkcija atlieka vienos strategijos testavimą, išmatuoja 
+ * vykdymo laiką ir atspausdina rezultatus.
+ * 
+ * @param strategijaPavadinimas Testuojamos strategijos pavadinimas
+ * @param studentai Studentų vektorius testavimui
+ * @param naudotiVidurki Parametras, nurodantis skaičiavimo metodą
+ */
 void testuotiStrategija(const string& strategijaPavadinimas,
                        vector<Studentas>& studentai,
                        bool naudotiVidurki);
 
+/**
+ * @brief Strategijų palyginimo funkcija
+ * 
+ * Ši funkcija atlieka visų trijų strategijų testavimą ir 
+ * palyginimą, išvedant kiekvienos strategijos rezultatus.
+ * 
+ * @param studentai Studentų vektorius testams
+ * @param naudotiVidurki Parametras, nurodantis skaičiavimo metodą
+ */
 void palygintiStrategijas(vector<Studentas>& studentai, 
                          bool naudotiVidurki);
 
