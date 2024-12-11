8f05268a9799383987bd345dfdd11c75f0024e04
eglejaseviciute <eglejasev@gmail.com>
2024-12-11 20:55:52 +0200

    Patvarkytas kodas

--- Class versija/Vector/Include/myLib.h
+++ Class versija/Vector/Include/myLib.h
@@ -1,22 +1,32 @@
+/**
+ * @file myLib.h
+ * @brief Pagrindinių bibliotekų ir naudingų funkcijų antraštės failas
+ * 
+ * Šis failas apima įvairias standartines C++ bibliotekas ir apibrėžia dažniausiai naudojamus
+ * duomenų tipus, funkcijas ir makrokomandas. Jis taip pat apima svarbiausias funkcijas,
+ * kuriomis gali naudotis kita programos dalis.
+ * 
+ */
+
 #ifndef MYLIB_H
 #define MYLIB_H
 
 
-#include <iostream>
-#include <vector>
-#include <string>
-#include <limits>
-#include <iomanip>
-#include <algorithm>
-#include <cstdlib>
-#include <fstream>
-#include <sstream>
-#include <cctype>
-#include <chrono>
-#include <functional>
-#include <map>
-#include <stdexcept>
-#include <numeric>
+#include <iostream> ///< Įtraukia funkcijas ir tipus įvesties ir išvesties operacijoms
+#include <vector>   ///< Įtraukia standartinį konteinerį 'vector'
+#include <string>   ///< Įtraukia string tipą ir su juo susijusias funkcijas
+#include <limits>   ///< Įtraukia standartinius limitus, pvz., 'numeric_limits'
+#include <iomanip>  ///< Įtraukia funkcijas, susijusias su įvesties/rašymo formatu
+#include <algorithm>///< Įtraukia algoritmus, tokius kaip 'sort', 'find' ir kt.
+#include <cstdlib>  ///< Įtraukia funkcijas, susijusias su atsitiktiniais skaičiais ir procesų valdymu
+#include <fstream>  ///< Įtraukia įvesties/išvesties funkcijas su failais
+#include <sstream>  ///< Įtraukia stringstream funkcijas
+#include <cctype>   ///< Įtraukia funkcijas simbolių apdorojimui
+#include <chrono>   ///< Įtraukia laiką susijusias funkcijas
+#include <functional>///< Įtraukia funkcijų objektus ir lambda funkcijas
+#include <map>      ///< Įtraukia žemėlapių konteinerį (key-value poros)
+#include <stdexcept>///< Įtraukia standartines išimtis
+#include <numeric>  ///< Įtraukia funkcijas, susijusias su skaičiavimais, pvz., 'accumulate'
 
 
 using std::endl;
